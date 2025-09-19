fda7f26d74566286e9c4f9a09dc947bdfbdba0f9
deliciouscat <sunmo009@gmail.com>
2025-09-19 16:36:27 +0900

    deploy.sh를 docs 폴더 방식에 맞게 수정

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-# Hugo 사이트 빌드
+# Hugo 사이트 빌드 (docs 폴더로 출력)
 echo "Building the website..."
-hugo -D
+hugo
 
-# public 디렉토리로 이동
-cd public
-
-# 변경사항 추가
+# 변경사항 추가 (docs 폴더와 소스 파일들 모두)
 git add .
 
 # 커밋 메시지 설정
@@ -16,22 +13,8 @@ if [ $# -eq 1 ]
   then msg="$1"
 fi
 
-# 커밋 및 푸시 (gh-pages 브랜치)
-git commit -m "$msg"
-git push origin gh-pages
-
-# 프로젝트 루트로 돌아가기
-cd ..
-
-# main 브랜치에도 변경사항 커밋 및 푸시
-git add .
-
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-
+# main 브랜치에 커밋 및 푸시 (GitHub Pages는 docs 폴더에서 자동 배포)
 git commit -m "$msg"
 git push origin main
 
-echo "배포가 완료되었습니다!"
+echo "배포가 완료되었습니다! GitHub Pages가 docs 폴더에서 자동으로 사이트를 배포합니다."
