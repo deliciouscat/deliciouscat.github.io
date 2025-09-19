c1db5f44630f9eeaf60c6d44347e58d8cd0c2c47
deliciouscat <sunmo009@gmail.com>
2025-09-19 16:37:12 +0900

    User Pages 방식으로 배포 설정 수정

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-# Hugo 사이트 빌드 (docs 폴더로 출력)
+# Hugo 사이트 빌드 (public 폴더로 출력)
 echo "Building the website..."
 hugo
 
-# 변경사항 추가 (docs 폴더와 소스 파일들 모두)
+# public 폴더의 내용을 root로 복사 (User Pages는 root에서 배포)
+echo "Copying public contents to root..."
+cp -r public/* .
+
+# 변경사항 추가
 git add .
 
 # 커밋 메시지 설정
@@ -13,8 +17,8 @@ if [ $# -eq 1 ]
   then msg="$1"
 fi
 
-# main 브랜치에 커밋 및 푸시 (GitHub Pages는 docs 폴더에서 자동 배포)
+# main 브랜치에 커밋 및 푸시 (User Pages는 root에서 자동 배포)
 git commit -m "$msg"
 git push origin main
 
-echo "배포가 완료되었습니다! GitHub Pages가 docs 폴더에서 자동으로 사이트를 배포합니다."
+echo "배포가 완료되었습니다! User Pages가 root 디렉토리에서 자동으로 사이트를 배포합니다."
